0f3ed7355f5b875ce9468187bc1133761e4825bb
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-25 22:58:49 -0400

    added routes to the products
    added routes to the products

--- projects/ecommerce-app-with-ngrx/src/app/app-routing.module.ts
+++ projects/ecommerce-app-with-ngrx/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/product-center', pathMatch: 'full' },
+  {
+    path: 'product-center',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- projects/ecommerce-app-with-ngrx/src/app/products/products-routing.module.ts
+++ projects/ecommerce-app-with-ngrx/src/app/products/products-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductCenterComponent } from './product-center/product-center.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductCenterComponent,
+    children: [
+      {
+        path: '',
+        component: ProductListComponent,
+        children: [
+          {
+            path: ':id',
+            component: ProductDetailComponent,
+          },
+        ],
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
