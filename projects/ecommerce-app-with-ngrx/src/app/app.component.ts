1168570885b136e24cf29844f0420b9736e4cf00
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-26 15:57:29 -0400

    logout and login reading data from store

--- projects/ecommerce-app-with-ngrx/src/app/app.component.ts
+++ projects/ecommerce-app-with-ngrx/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './reducers';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { stat } from 'fs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loading = false;
   title = 'ecommerce app with NGRX';
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.isLoggedIn$ = this.store.pipe(map((state) => !!state['auth'].user));
+    this.isLoggedOut$ = this.store.pipe(map((state) => !state['auth'].user));
+  }
+
   logout() {}
 }
