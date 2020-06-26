a7fa40f1ca6f426546b73279fbd5b521fb8f5da4
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-25 21:36:34 -0400

    added sidenav to ecommerce app
    added sidenav to ecommerce app

--- projects/ecommerce-app-with-ngrx/src/app/app.component.ts
+++ projects/ecommerce-app-with-ngrx/src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ecommerce-app-with-ngrx';
+  loading = false;
+  title = 'ecommerce app with NGRX';
+  logout() {}
 }
--- projects/ecommerce-app-with-ngrx/src/app/app.module.ts
+++ projects/ecommerce-app-with-ngrx/src/app/app.module.ts
@@ -4,17 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
