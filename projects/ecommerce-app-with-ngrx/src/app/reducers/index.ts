ed651d73e442c53c536a480db7874ab4eb0cd01b
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-27 19:35:11 -0400

    implemented logger metareducer

--- projects/ecommerce-app-with-ngrx/src/app/reducers/index.ts
+++ projects/ecommerce-app-with-ngrx/src/app/reducers/index.ts
@@ -14,6 +14,14 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer,
 };
 
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state before:', state);
+    console.log('current action', action);
+    return reducer(state, action);
+  };
+}
+
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
-  ? []
+  ? [logger]
   : [];
