85357295725d7291f8650281a4d581ecf1ebc605
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-28 10:02:03 -0400

    added admin navbar

--- projects/ecommerce-app-with-ngrx/src/app/admin/admin.module.ts
+++ projects/ecommerce-app-with-ngrx/src/app/admin/admin.module.ts
@@ -5,9 +5,22 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AdminCenterComponent } from './admin-center/admin-center.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AddProductComponent, AdminCenterComponent],
-  imports: [CommonModule, AdminRoutingModule, MatToolbarModule],
+  declarations: [
+    AddProductComponent,
+    AdminCenterComponent,
+    AdminNavbarComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class AdminModule {}
