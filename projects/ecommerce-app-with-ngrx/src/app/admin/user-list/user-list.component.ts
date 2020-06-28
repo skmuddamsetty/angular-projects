e2b1ca0d3036c52d28f7099bc1097a4b8a585aaa
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-06-28 18:15:18 -0400

    users list html

--- projects/ecommerce-app-with-ngrx/src/app/admin/user-list/user-list.component.ts
+++ projects/ecommerce-app-with-ngrx/src/app/admin/user-list/user-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +9,10 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+  users$: Observable<User[]>;
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userService.getAllUsers().subscribe(console.log);
+    this.users$ = this.userService.getAllUsers();
   }
 }
