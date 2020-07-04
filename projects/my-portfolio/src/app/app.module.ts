d6fab206d0476a2d76c7c894ca39d3ae1981e04d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-04 09:49:53 -0400

    modified some changes

--- projects/my-portfolio/src/app/app.module.ts
+++ projects/my-portfolio/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
