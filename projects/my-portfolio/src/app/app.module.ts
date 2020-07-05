82d354b57b41a5691de4224f9975984b7793524b
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-04 23:46:57 -0400

    added experience components and timeline changes
    added experience components and timeline changes

--- projects/my-portfolio/src/app/app.module.ts
+++ projects/my-portfolio/src/app/app.module.ts
@@ -10,9 +10,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
-  declarations: [AppComponent, WorksComponent],
+  declarations: [AppComponent, WorksComponent, ExperienceComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
