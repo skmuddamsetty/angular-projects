b164f385608fe2ab4aa4dc68fe640c1acbbccc1f
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-07 23:51:25 -0400

    completed typing test project
    completed typing test project

--- projects/typing-challenge/src/app/app.component.ts
+++ projects/typing-challenge/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomText = lorem.sentence();
+  enteredText = '';
+  onInput(val: string) {
+    this.enteredText = val;
+  }
+
+  compare(char: string, enteredText: string) {
+    if (!enteredText) {
+      return 'pending';
+    }
+    return char === enteredText ? 'correct' : 'incorrect';
+  }
 }
