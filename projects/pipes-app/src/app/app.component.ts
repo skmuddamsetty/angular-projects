98879ab20c168c368094a734d801ff4e43898f83
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-09 08:27:16 -0400

    pipes app till currency pipe
    pipes app till currency pipe

---
+++ projects/pipes-app/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+}
