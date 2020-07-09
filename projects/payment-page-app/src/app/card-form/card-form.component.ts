9d791543714b0e92be5208f4a63d9848f01bc818
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-08 22:50:40 -0400

    completed custome date form control
    completed custome date form control

--- projects/payment-page-app/src/app/card-form/card-form.component.ts
+++ projects/payment-page-app/src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-card-form',
@@ -14,8 +15,26 @@ export class CardFormComponent implements OnInit {
       // Validators.maxLength(5),
       // Validators.pattern(/\s/),
     ]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('form submitted with values', this.cardForm.value);
+  }
 }
