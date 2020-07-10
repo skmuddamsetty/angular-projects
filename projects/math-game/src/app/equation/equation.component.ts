31e83b5d7215229aa2ae78c922585cc0f22c47c6
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-09 23:15:21 -0400

    middle of 253
    middle of 253

--- projects/math-game/src/app/equation/equation.component.ts
+++ projects/math-game/src/app/equation/equation.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.scss']
+  styleUrls: ['./equation.component.scss'],
 })
 export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.randomNumber()),
+    b: new FormControl(this.randomNumber()),
+    answer: new FormControl(''),
+  });
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
   }
 
+  get a() {
+    return this.mathForm.value.a;
+  }
 }
