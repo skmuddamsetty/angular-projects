df7f25c10e08f3ecceac90ba38a43a64342dd2ae
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-10 21:44:48 -0400

    added styling to the project and styling to the signup form
    added styling to the project and styling to the signup form

--- projects/email-client/src/app/auth/auth.module.ts
+++ projects/email-client/src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SignInComponent, SignUpComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- projects/email-client/src/app/auth/sign-up/sign-up.component.ts
+++ projects/email-client/src/app/auth/sign-up/sign-up.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  authForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    passwordConfirmation: new FormControl('', [Validators.required]),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
