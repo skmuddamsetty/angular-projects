b2be98099cb764806737eaba24428bc69ee41ac7
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-11 16:44:19 -0400

    completed adding functionality to signup form
    completed adding functionality to signup form

--- projects/email-client/src/app/auth/auth.service.ts
+++ projects/email-client/src/app/auth/auth.service.ts
@@ -6,18 +6,37 @@ interface UsernameAvailableResponse {
   available: boolean;
 }
 
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+interface SignupResponse {
+  username: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com';
+
   constructor(private http: HttpClient) {}
 
   userNameAvailable(username: string): Observable<UsernameAvailableResponse> {
     return this.http.post<UsernameAvailableResponse>(
-      'https://api.angular-email.com/auth/username',
+      `${this.rootUrl}/auth/username`,
       {
         username,
       }
+    );
+  }
+
+  signup(credentials: SignupCredentials): Observable<SignupResponse> {
+    return this.http.post<SignupResponse>(
+      `${this.rootUrl}/auth/signup`,
+      credentials
     );
   }
 }
--- projects/email-client/src/app/auth/sign-up/sign-up.component.ts
+++ projects/email-client/src/app/auth/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -41,8 +42,26 @@ export class SignUpComponent implements OnInit {
   );
   constructor(
     private matchPassword: MatchPassword,
-    private uniqueUsername: UniqueUsername
+    private uniqueUsername: UniqueUsername,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.authForm.invalid) return;
+    this.authService.signup(this.authForm.value).subscribe({
+      next: (response) => {
+        // Navigate to some other route
+      },
+      // complete() {},
+      error: (err) => {
+        if (err.status === 0) {
+          this.authForm.setErrors({ noConnection: true });
+        } else {
+          this.authForm.setErrors({ unknownError: true });
+        }
+      },
+    });
+  }
 }
