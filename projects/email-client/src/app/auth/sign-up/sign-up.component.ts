630793151da7c01e5c92ee36cc8db7a6107ff85d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-10 23:02:19 -0400

    added custome validator for password match
    added custome validator for password match

--- projects/email-client/src/app/auth/sign-up/sign-up.component.ts
+++ projects/email-client/src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,12 +8,30 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-  authForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    passwordConfirmation: new FormControl('', [Validators.required]),
-  });
-  constructor() {}
+  authForm = new FormGroup(
+    {
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.pattern(/^[a-z0-9]+$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+      passwordConfirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+    },
+    {
+      validators: [this.matchPassword.validate],
+    }
+  );
+  constructor(private matchPassword: MatchPassword) {}
 
   ngOnInit(): void {}
 }
---
+++ projects/email-client/src/app/auth/validators/match-password.ts
@@ -0,0 +1,13 @@
+import { Validator, FormGroup } from '@angular/forms';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class MatchPassword implements Validator {
+  validate(formGroup: FormGroup) {
+    const { password, passwordConfirmation } = formGroup.value;
+    if (password === passwordConfirmation) {
+      return null;
+    }
+    return { passwordDontMatch: true };
+  }
+}
