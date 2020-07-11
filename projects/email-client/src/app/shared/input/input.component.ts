dd7a7a233ca4288a2fe195b7869245e4460528b8
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-11 16:11:14 -0400

    completed all things about signup form
    completed all things about signup form

--- projects/email-client/src/app/shared/input/input.component.ts
+++ projects/email-client/src/app/shared/input/input.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
+  @Input() label: string;
+  @Input() control: FormControl;
+  @Input() inputType: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
   }
-
 }
