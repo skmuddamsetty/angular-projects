5a92b0f1c4f51a486c9c201d0ee814b33187b7de
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-13 08:48:51 -0400

    added auth guard
    added auth guard

--- projects/email-client/src/app/app-routing.module.ts
+++ projects/email-client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'inbox',
     loadChildren: () =>
       import('./inbox/inbox.module').then((module) => module.InboxModule),
+    canLoad: [AuthGuard],
   },
 ];
 
