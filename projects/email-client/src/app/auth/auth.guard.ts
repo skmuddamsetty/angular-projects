5fd409b73c2ad3048772320e22930201f8aa7232
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-13 10:12:33 -0400

    completed authentication part
    completed authentication part

--- projects/email-client/src/app/auth/auth.guard.ts
+++ projects/email-client/src/app/auth/auth.guard.ts
@@ -7,13 +7,18 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   UrlTree,
+  Router,
 } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { skipWhile, take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -28,6 +33,14 @@ export class AuthGuard implements CanActivate, CanLoad {
     route: Route,
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.authService.signedIn$.pipe(
+      skipWhile((value) => value === null),
+      take(1),
+      tap((authenticated) => {
+        if (!authenticated) {
+          this.router.navigateByUrl('/');
+        }
+      })
+    );
   }
 }
--- projects/email-client/src/app/auth/auth.service.ts
+++ projects/email-client/src/app/auth/auth.service.ts
@@ -36,7 +36,7 @@ interface SignedInResponse {
 })
 export class AuthService {
   rootUrl = 'https://api.angular-email.com';
-  signedIn$ = new BehaviorSubject(false);
+  signedIn$ = new BehaviorSubject(null);
 
   constructor(private http: HttpClient) {}
 
