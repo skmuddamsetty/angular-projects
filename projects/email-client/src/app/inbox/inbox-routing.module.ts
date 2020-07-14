5bb903be947d910a4a0c3b74a828e03946c8efde
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-14 17:35:54 -0400

    added placeholder component and child routes for inbox
    added placeholder component and child routes for inbox

--- projects/email-client/src/app/inbox/inbox-routing.module.ts
+++ projects/email-client/src/app/inbox/inbox-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children: [
+      { path: ':id', component: EmailShowComponent },
+      {
+        path: '',
+        component: PlaceholderComponent,
+      },
+    ],
   },
 ];
 
