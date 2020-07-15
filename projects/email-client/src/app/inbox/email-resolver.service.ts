526bd3ce69cd6594027141fdbba3768e48281fe3
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-07-15 13:17:16 -0400

    error handling in email resolver

--- projects/email-client/src/app/inbox/email-resolver.service.ts
+++ projects/email-client/src/app/inbox/email-resolver.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Email } from './email';
 import { EmailService } from './email.service';
+import { catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailResolverService implements Resolve<Email> {
-  constructor(private emailService: EmailService) {}
+  constructor(private emailService: EmailService, private router: Router) {}
   resolve(route: ActivatedRouteSnapshot) {
     const { id } = route.params;
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+        return EMPTY;
+      })
+    );
   }
 }
