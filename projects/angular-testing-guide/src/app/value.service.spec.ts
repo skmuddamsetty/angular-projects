ecf9643fb4d2c01b26a84f9dc484a631f4df066d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-25 07:46:47 -0500

    synchronous and asynchronous unit tests of the ValueService written without assistance from Angular testing utilities.

--- projects/angular-testing-guide/src/app/value.service.spec.ts
+++ projects/angular-testing-guide/src/app/value.service.spec.ts
@@ -1,16 +1,38 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ValueService } from './value.service';
 
 describe('ValueService', () => {
   let service: ValueService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ValueService);
+    service = new ValueService();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getValue should return real value', () => {
+    expect(service.getValue()).toBe('real value');
+  });
+
+  it('#getObservableValue should return value from observable', (done: DoneFn) => {
+    service.getObservableValue().subscribe((value) => {
+      expect(value).toBe('observable value');
+      done();
+    });
+  });
+
+  it('#getPromiseValue should return value from a promise', (done: DoneFn) => {
+    service.getPromiseValue().then((value) => {
+      expect(value).toBe('promise value');
+      done();
+    });
+  });
+
+  it('#getObservableDelayValue should return value from a observable after a delay', (done: DoneFn) => {
+    service.getObservableDelayValue().subscribe((value) => {
+      expect(value).toBe('observable delay value');
+      done();
+    });
   });
 });
