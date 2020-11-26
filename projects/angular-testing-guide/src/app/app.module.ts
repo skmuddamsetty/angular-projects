0bb689aec1d9365b344d5dd30cbb28ebbc32c200
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-26 09:51:06 -0500

    created welcome component to test a component which is dependent on a service

--- projects/angular-testing-guide/src/app/app.module.ts
+++ projects/angular-testing-guide/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LightSwitchComponent } from './light-switch/light-switch.component';
 import { DashboardHeroComponent } from './dashboard-hero/dashboard-hero.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LightSwitchComponent,
-    DashboardHeroComponent
+    DashboardHeroComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/angular-testing-guide/src/app/welcome/welcome.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WelcomeComponent } from './welcome.component';
+
+class MockUserService {
+  isLoggedIn = true;
+  user = { name: 'Test User' };
+}
+
+describe('WelcomeComponent', () => {
+  let component: WelcomeComponent;
+  let fixture: ComponentFixture<WelcomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [WelcomeComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WelcomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
