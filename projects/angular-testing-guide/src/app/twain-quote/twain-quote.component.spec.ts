257bfefd25554c18c5944ac3171f1d3c7abd2534
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-27 00:38:52 -0500

    https://angular.io/guide/testing-components-scenarios#synchronous-tests

--- projects/angular-testing-guide/src/app/twain-quote/twain-quote.component.spec.ts
+++ projects/angular-testing-guide/src/app/twain-quote/twain-quote.component.spec.ts
@@ -1,25 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
-import { TwainQuoteComponent } from './twain-quote.component';
+import { TwainComponent } from './twain-quote.component';
+import { TwainQuoteService } from './twain-quote.service';
 
 describe('TwainQuoteComponent', () => {
-  let component: TwainQuoteComponent;
-  let fixture: ComponentFixture<TwainQuoteComponent>;
+  let component: TwainComponent;
+  let fixture: ComponentFixture<TwainComponent>;
+  let getQuoteSpy: jasmine.Spy;
+  let testQuote: string;
+  let quoteEl: HTMLElement;
 
   beforeEach(async(() => {
+    testQuote = 'Test Quote';
+
+    // Create a fake TwainService object with a `getQuote()` spy
+    const twainService = jasmine.createSpyObj('TwainService', ['getQuote']);
+    // Make the spy return a synchronous Observable with the test data
+    getQuoteSpy = twainService.getQuote.and.returnValue(of(testQuote));
     TestBed.configureTestingModule({
-      declarations: [ TwainQuoteComponent ]
-    })
-    .compileComponents();
+      declarations: [TwainComponent],
+      providers: [{ provide: TwainQuoteService, useValue: twainService }],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TwainQuoteComponent);
+    fixture = TestBed.createComponent(TwainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    quoteEl = fixture.nativeElement.querySelector('.twain');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show quote after component initialized', () => {
+    fixture.detectChanges(); // onInit()
+
+    // sync spy result shows testQuote immediately after init
+    expect(quoteEl.textContent).toBe(testQuote);
+    expect(getQuoteSpy.calls.any()).toBe(true, 'getQuote called');
   });
 });
--- projects/angular-testing-guide/src/app/twain-quote/twain-quote.service.spec.ts
+++ projects/angular-testing-guide/src/app/twain-quote/twain-quote.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { TwainQuoteService } from './twain-quote.service';
+// import { TwainQuoteService } from './twain-quote.service';
 
-describe('TwainQuoteService', () => {
-  let service: TwainQuoteService;
+// describe('TwainQuoteService', () => {
+//   let service: TwainQuoteService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TwainQuoteService);
-  });
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({});
+//     service = TestBed.inject(TwainQuoteService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
+// });
