5d1bafa8afe09b3abaae274ab4317ef6fad193ee
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-27 21:22:22 -0500

    completed crud operations

--- projects/angular-tour-of-heroes/src/app/hero.service.ts
+++ projects/angular-tour-of-heroes/src/app/hero.service.ts
@@ -16,6 +16,10 @@ export class HeroService {
     private messageService: MessageService
   ) {}
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   // getHeroes(): Observable<Hero[]> {
   //   // TODO: send the message _after_ fetching the heroes
   //   this.messageService.add('HeroService: fetched heroes');
@@ -67,5 +71,32 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
   }
 }
