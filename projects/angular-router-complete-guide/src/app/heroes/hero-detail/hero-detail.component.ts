2d1ba0c16593e49630a748ca8f91a017c1886322
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-28 08:23:51 -0500

    completed https://angular.io/guide/router-tutorial-toh#heroes-list-optionally-selecting-a-hero

--- projects/angular-router-complete-guide/src/app/heroes/hero-detail/hero-detail.component.ts
+++ projects/angular-router-complete-guide/src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,27 +12,41 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero$: Observable<Hero>;
+
   constructor(
     private route: ActivatedRoute,
-    private heroService: HeroService,
-    private location: Location
+    private router: Router,
+    private service: HeroService
   ) {}
 
-  ngOnInit(): void {
-    this.getHero();
+  ngOnInit() {
+    this.hero$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => this.service.getHero(params.get('id')))
+    );
   }
 
-  getHero(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
-  }
+  /**
+   * https://angular.io/guide/router-tutorial-toh#snapshot-the-no-observable-alternative
+   * @param hero
+   */
+  // ngOnInit() {
+  //   const id = this.route.snapshot.paramMap.get('id');
 
-  goBack(): void {
-    this.location.back();
-  }
+  //   this.hero$ = this.service.getHero(id);
+  // }
 
-  save(): void {
-    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  gotoHeroes(hero: Hero) {
+    const heroId = hero ? hero.id : null;
+    // Pass along the hero id if available
+    // so that the HeroList component can select that hero.
+    // Include a junk 'foo' property for fun.
+    this.router.navigate(['/heroes', { id: heroId, foo: 'foo' }]);
   }
 }
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
