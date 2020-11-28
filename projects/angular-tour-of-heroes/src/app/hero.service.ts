2a699214dc0483308ec435d217978b602e3b5a23
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-11-27 20:19:32 -0500

    completed till https://angular.io/tutorial/toh-pt6#httpclientget-returns-response-data

--- projects/angular-tour-of-heroes/src/app/hero.service.ts
+++ projects/angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,22 +3,37 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  private heroesUrl = 'api/heroes'; // URL to web api
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
+  // getHeroes(): Observable<Hero[]> {
+  //   // TODO: send the message _after_ fetching the heroes
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find((hero) => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
